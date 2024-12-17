7f4dddc3d2f4731ab67a0a2dbc70c7b9de874d22
Anytaloban <anytaloban@gmail.com>
2024-12-17 10:31:09 +0200

    SingleEvent.h

--- lab 3/SingleEvent.h
+++ lab 3/SingleEvent.h
@@ -1,1 +1,19 @@
-#pragma once
+#ifndef SINGLEEVENT_H
+#define SINGLEEVENT_H
+
+#include "Event.h"
+#include <string>
+using namespace std;
+
+class SingleEvent: public Event
+{
+private:
+	int date[3];
+public:
+	SingleEvent();
+	SingleEvent(string& name, double duration, int day, int month, int year);
+	void displayEvent();
+	SingleEvent& inputEventDetails();
+};
+
+#endif
