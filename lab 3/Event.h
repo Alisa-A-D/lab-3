7a524dec7aef2deb51c2e3b086bc04c05b5cdac0
Anytaloban <anytaloban@gmail.com>
2024-12-17 10:30:17 +0200

    Event.h

--- lab 3/Event.h
+++ lab 3/Event.h
@@ -1,1 +1,21 @@
-#pragma once
+#ifndef EVENT_H
+#define EVENT_H
+
+#include <string>
+using namespace std;
+
+class Event 
+{
+protected:
+	string name;
+	double duration;
+public:
+	Event();
+	Event(string& name, double  duration);
+	virtual void displayEvent() = 0;
+	string& getName();
+	double getDuration();
+	virtual Event& inputEventDetails() = 0;
+};
+
+#endif
