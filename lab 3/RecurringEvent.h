165f1386daf78d46a25549239f44139bed21550f
Anytaloban <anytaloban@gmail.com>
2024-12-17 10:32:36 +0200

    RecurringEvent.h

--- lab 3/RecurringEvent.h
+++ lab 3/RecurringEvent.h
@@ -1,1 +1,22 @@
-#pragma once
+#ifndef RECURRINGEVENT_H
+#define RECURRINGEVENT_H
+
+#include "Event.h"
+#include <string>
+using namespace std;
+
+class RecurringEvent : public Event
+{
+private:
+	int startDate[3];
+	int endDate[3];
+	int repeats;
+public:
+	RecurringEvent();
+	RecurringEvent(string& name, double duration, int sDay, int sMonth, int sYear, int eDay, int eMonth, int eYear, int repeats);
+	void displayEvent();
+	int getRepeats();
+	RecurringEvent& inputEventDetails();
+};
+
+#endif
