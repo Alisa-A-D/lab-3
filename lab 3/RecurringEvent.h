e89ebe4c8ed4e498d054324b50f0b739aa468f94
Alisa-A-D <dukhopel.alisa@lll.kpi.ua>
2024-12-19 20:44:22 +0200

    Update RecurringEvent.h

--- lab 3/RecurringEvent.h
+++ lab 3/RecurringEvent.h
@@ -2,21 +2,27 @@
 #define RECURRINGEVENT_H
 
 #include "Event.h"
-#include <string>
-using namespace std;
 
 class RecurringEvent : public Event
 {
 private:
-	int startDate[3];
-	int endDate[3];
-	int repeats;
+    int startDate[3];
+    int endDate[3];
+    int repeats;
+
 public:
-	RecurringEvent();
-	RecurringEvent(string& name, double duration, int sDay, int sMonth, int sYear, int eDay, int eMonth, int eYear, int repeats);
-	void displayEvent();
-	int getRepeats();
-	RecurringEvent& inputEventDetails();
+    RecurringEvent();
+    RecurringEvent(const string& name, double duration, int sDay, int sMonth, int sYear,
+        int eDay, int eMonth, int eYear, int repeats);
+
+    void displayEvent() const override;
+    int getRepeats() const;
+    RecurringEvent& inputEventDetails() override;
+
+
+    const int* getStartDate() const; 
+    const int* getEndDate() const;   
+
 };
 
 #endif
