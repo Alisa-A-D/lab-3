ac5bf7094471d5a6d3c6f464d6a37eea0c23221f
Alisa-A-D <dukhopel.alisa@lll.kpi.ua>
2024-12-19 20:43:02 +0200

    Update FileManager.cpp

--- lab 3/FileManager.cpp
+++ lab 3/FileManager.cpp
@@ -1,35 +1,81 @@
 #include "FileManager.h"
-#include "SingleEvent.h" 
+#include "SingleEvent.h"
+#include "RecurringEvent.h"
 #include <fstream>
 #include <iostream>
 using namespace std;
 
-bool FileManager::saveEventsToFile(const vector<Event*>& events, const string& filename) {
+bool FileManager::saveEventsToFile(const vector<Event*>& events, const string& filename)
+{
     ofstream outFile(filename);
-    if (!outFile) {
-        cerr << "Error" << endl;
-        return false;
+    if (!outFile) return false;
+
+    for (const auto& event : events)
+    {
+        auto singleEvent = dynamic_cast<SingleEvent*>(event);
+        if (singleEvent) {
+            outFile << "SingleEvent "
+                << singleEvent->getName() << " "
+                << singleEvent->getDuration() << " "
+                << singleEvent->getDate()[0] << " "
+                << singleEvent->getDate()[1] << " "
+                << singleEvent->getDate()[2] << endl;
+        }
+        else {
+            auto recurringEvent = dynamic_cast<RecurringEvent*>(event);
+            if (recurringEvent) {
+                outFile << "RecurringEvent "
+                    << recurringEvent->getName() << " "
+                    << recurringEvent->getDuration() << " "
+                    << recurringEvent->getStartDate()[0] << " "
+                    << recurringEvent->getStartDate()[1] << " "
+                    << recurringEvent->getStartDate()[2] << " "
+                    << recurringEvent->getEndDate()[0] << " "
+                    << recurringEvent->getEndDate()[1] << " "
+                    << recurringEvent->getEndDate()[2] << " "
+                    << recurringEvent->getRepeats() << endl;
+            }
+        }
     }
-    for (const auto& event : events) {
-        outFile << event->getName() << "|" << event->getDuration() << endl;
-    }
-    outFile.close();
+
     return true;
 }
 
-bool FileManager::loadEventsFromFile(vector<Event*>& events, const string& filename) {
+
+bool FileManager::loadEventsFromFile(vector<Event*>& events, const string& filename)
+{
     ifstream inFile(filename);
-    if (!inFile) {
-        cerr << "Error" << endl;
-        return false;
-    }
+    if (!inFile) return false;
 
     events.clear();
-    string name;
-    double duration;
-    while (inFile >> ws && getline(inFile, name, '|') && inFile >> duration) {
-        events.push_back(new SingleEvent(name, duration)); 
+    string type;
+
+    while (inFile >> type) {
+        if (type == "SingleEvent") {
+            string name;
+            double duration;
+            int day, month, year;
+
+            inFile >> ws; 
+            getline(inFile, name, ' ');
+            inFile >> duration >> day >> month >> year;
+
+            events.push_back(new SingleEvent(name, duration, day, month, year));
+        }
+        else if (type == "RecurringEvent") {
+            string name;
+            double duration;
+            int sDay, sMonth, sYear, eDay, eMonth, eYear, repeats;
+
+            inFile >> ws; 
+            getline(inFile, name, ' ');
+            inFile >> duration >> sDay >> sMonth >> sYear
+                >> eDay >> eMonth >> eYear >> repeats;
+
+            events.push_back(new RecurringEvent(name, duration, sDay, sMonth, sYear, eDay, eMonth, eYear, repeats));
+        }
     }
-    inFile.close();
+
     return true;
 }
+
