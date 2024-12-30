141178ee942137aaf6b6a2a690f7424b148b9094
Anytaloban <anytaloban@gmail.com>
2024-12-30 02:42:01 +0200

    Update Event1.py

--- python/Event1.py
+++ python/Event1.py
@@ -1,1 +1,20 @@
+from abc import ABC, abstractmethod
 
+class Event(ABC):
+    def __init__(self, line, dur):
+        self._name=line
+        self._duration=dur
+        
+    def get_name(self):
+        return self._name
+            
+    def get_duration(self):
+        return self._duration
+        
+    @abstractmethod
+    def display_event(self):
+        pass
+        
+    @abstractmethod
+    def input_event_details(self):
+        pass
