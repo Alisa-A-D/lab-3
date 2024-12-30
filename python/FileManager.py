c8d2c55e549b3b96c755459c238121a8edf11818
Anytaloban <anytaloban@gmail.com>
2024-12-30 14:09:47 +0200

    Update FileManager.py

--- python/FileManager.py
+++ python/FileManager.py
@@ -1,6 +1,5 @@
 from SingleEvent import SingleEvent
-from RecurringEvent import RecurringEvent
-
+from ReccuringEvent import ReccuringEvent
 
 class FileManager:
 
@@ -10,8 +9,8 @@ class FileManager:
             for event in events:
                 if isinstance(event, SingleEvent):
                     out_file.write(f"SingleEvent {event.get_name()} {event.get_duration()} {event.get_date()[0]} {event.get_date()[1]} {event.get_date()[2]}\n")
-                elif isinstance(event, RecurringEvent):
-                    out_file.write(f"RecurringEvent {event.get_name()} {event.get_duration()} {event.get_start_date()[0]} {event.get_start_date()[1]} {event.get_start_date()[2]} "
+                elif isinstance(event, ReccuringEvent):
+                    out_file.write(f"ReccuringEvent {event.get_name()} {event.get_duration()} {event.get_start_date()[0]} {event.get_start_date()[1]} {event.get_start_date()[2]} "
                                    f"{event.get_end_date()[0]} {event.get_end_date()[1]} {event.get_end_date()[2]} {event.get_repeats()}\n")
         return True
 
@@ -27,13 +26,13 @@ class FileManager:
                     name = parts[1]
                     duration = float(parts[2])
                     day, month, year = map(int, parts[3:6])
-                    events.append(SingleEvent(name, duration, day, month, year))
+                    events.append(SingleEvent().change_event(name, duration, day, month, year))
 
-                elif event_type == "RecurringEvent":
+                elif event_type == "ReccuringEvent":
                     name = parts[1]
                     duration = float(parts[2])
                     s_day, s_month, s_year = map(int, parts[3:6])
                     e_day, e_month, e_year = map(int, parts[6:9])
                     repeats = int(parts[9])
-                    events.append(RecurringEvent(name, duration, s_day, s_month, s_year, e_day, e_month, e_year, repeats))
+                    events.append(ReccuringEvent().change_event(name, duration, s_day, s_month, s_year, e_day, e_month, e_year, repeats))
         return True
